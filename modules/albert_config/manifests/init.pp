281045bbc410da75f6a1051f39659083b90cd763
Gatlin Newhouse <gatlinnewhouse@gmail.com>
2017-02-06 15:03:55 -0800

    Created a symlink in one config
    Using [this](https://coderwall.com/p/yfnesa/create-a-symlink-in-puppet) as an example.

--- modules/albert_config/manifests/init.pp
+++ modules/albert_config/manifests/init.pp
@@ -2,6 +2,7 @@ class albert_config {
 
   file { "/home/$id/.config/albert/albert.conf":
     owner => "$id",
+    ensure => 'link',
     group => "$id",
     mode => "644",
     source => "puppet:///modules/albert_config/albert.conf"
