19e5098dfa00269c0a318ea31c6f6cab4bd59b38
gatlinnewhouse <gatlinnewhouse@gmail.com>
2017-05-11 04:03:16 -0700

    Adding pkgInstallDateLister script from bbs.archlinux.org thread And deprecating i3lock

--- .scripts/lock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 25% -scale 400% -fill black -colorize 75% /tmp/screen.png
-convert /tmp/screen.png /home/delueze/.dotfiles/scripts/ -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
